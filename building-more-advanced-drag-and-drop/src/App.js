196e1d47308ae0f39df8479b87faf1b9325f653b
Bobovino <rodrigomartineztabernero97@gmail.com>
2023-09-11 18:47:05 +0200

    Adding columns

--- building-more-advanced-drag-and-drop/src/App.js
+++ building-more-advanced-drag-and-drop/src/App.js
@@ -1,41 +1,63 @@
 import React, { useState } from 'react';
 import TaskList from './Components/tasklist';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { id: '0', name: 'Task 1' },
-    { id: '1', name: 'Task 2' },
-    { id: '2', name: 'Task 3' },
-  ]);
+  const [tasks, setTasks] = useState({
+    "todo": [
+      { id: '1', content: 'Task 1' },
+      { id: '2', content: 'Task 2' }
+    ],
+    "inProgress": [
+      { id: '3', content: 'Task 3' },
+    ],
+    "done": [
+      { id: '4', content: 'Task 4' },
+    ]
+  });
 
-  const onDragEnd = (result) => {
-   const { destination, source, draggableId } = result;
+  const handleOnDragEnd = (result) => {
+    const { source, destination } = result;
+    if (!destination) return;
+    if (source.droppableId === destination.droppableId && source.index === destination.index) return;
 
-  if (!destination) {
-    return;
-  }
+    // Create a copy of the item before removing it from state
+    const itemCopy = { ...tasks[source.droppableId][source.index] };
 
-  if (
-    destination.droppableId === source.droppableId &&
-    destination.index === source.index
-  ) {
-    return;
-  }
+    // Remove the item from the source list and insert it into the destination
+    setTasks(prev => {
+      prev = { ...prev };
+      prev[source.droppableId].splice(source.index, 1);
+      prev[destination.droppableId].splice(destination.index, 0, itemCopy);
 
-  const newTasks = Array.from(tasks);
-  const [reorderedTask] = newTasks.splice(source.index, 1);
-  newTasks.splice(destination.index, 0, reorderedTask);
-
-  setTasks(newTasks);
- };
-
+      return prev;
+    });
+  };
   return (
     <div className="App">
-      <h1>Task App</h1>
-      <DragDropContext onDragEnd={onDragEnd}>  
-            <TaskList tasks={tasks} />
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        {Object.entries(tasks).map(([column, tasks]) => (
+          <div key={column}>
+            <h2>{column}</h2>
+            <Droppable droppableId={column}>
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {tasks.map(({ id, content }, index) => (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          {content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </div>
+        ))}
       </DragDropContext>
     </div>
   );
